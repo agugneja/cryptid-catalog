3e86adf92dad15fbc17c23d31248c35418c7c04d
Alyssa Gugneja <alyssagugneja@gmail.com>
2022-10-29 19:35:32 -0400

    updating login and register pages with minimal css

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ order_dict = {}
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('home.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
