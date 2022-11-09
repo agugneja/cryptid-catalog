0a36d266e4aa9cc312b9c53bb9957d177e1ec3a7
alyssa_robinson <arobinson0227@gmail.com>
2022-11-09 16:01:46 -0500

    commit the database

---
+++ database_tables.sql
@@ -0,0 +1,46 @@
+CREATE TABLE IF NOT EXISTS Person (
+	user_id SERIAL,
+	username VARCHAR(255) NOT NULL,
+	password VARCHAR(255) NOT NULL,
+	PRIMARY KEY (user_id)
+);
+
+CREATE TABLE IF NOT EXISTS Post (
+	post_id SERIAL,
+	title VARCHAR(255) NOT NULL,
+	creature VARCHAR(255) NOT NULL,
+	date_time TIMESTAMP NOT NULL,
+	user_id INT NOT NULL,
+	likes INT NOT NULL,
+	dislikes INT NOT NULL,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (user_id) REFERENCES Person(user_id)
+);
+
+CREATE TABLE IF NOT EXISTS  Sighting (
+	post_id SERIAL,
+	place VARCHAR(255) NOT NULL,
+	description TEXT, 
+	photo BYTEA,
+	PRIMARY KEY (post_id),
+	FOREIGN KEY (post_id) REFERENCES Post(post_id)
+);
+
+CREATE TABLE IF NOT EXISTS Encyclopedia (
+	post_id SERIAL,
+	text_entry TEXT,
+	PRIMARY KEY(post_id),
+	FOREIGN KEY (post_id) REFERENCES Post(post_id)
+);
+
+CREATE TABLE IF NOT EXISTS Comment (
+	comment_id SERIAL,
+	post_id SERIAL,
+	text TEXT NOT NULL,
+	user_id SERIAL,
+	likes INT NOT NULL,
+	dislikes INT NOT NULL,
+	PRIMARY KEY(comment_id),
+	FOREIGN KEY (post_id) REFERENCES Post(post_id),
+	FOREIGN KEY (user_id) REFERENCES Person(user_id)
+);
