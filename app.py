69eb62bb7d488dbb91d29600777cb5a57e7fce01
alyssa_robinson <arobinson0227@gmail.com>
2022-11-09 15:57:39 -0500

    commit changes

--- app.py
+++ app.py
@@ -6,3 +6,18 @@ order_dict = {}
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('home.html')
+@app.get('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.get('/encyclopedia')
+def encyclopedia():
+    return render_template('encyclopedia.html')
+
+@app.get('/sightings')
+def sightings():
+    return render_template('sightings.html')
+
+@app.get('/logout')
+def logout():
+    return render_template('login.html')
