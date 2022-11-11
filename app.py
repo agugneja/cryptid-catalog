02b18d41e3ccae6f22cf837a70c2451989f9836e
Alyssa Gugneja <alyssagugneja@gmail.com>
2022-11-10 19:35:02 -0500

    adding basic post creation

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, abort, redirect
 from dotenv import load_dotenv
 import os
+from datetime import datetime
+from src.models import Post, db
 
 load_dotenv()
 
@@ -23,6 +25,25 @@ def encyclopedia():
 @app.get('/sightings')
 def sightings():
     return render_template('sightings.html')
+
+def create_post():
+    title = request.args.get('title')
+    creature = request.args.get('creature')
+    description = request.args.get('description')
+    user_id = 1
+    likes = 0
+    dislikes = 0
+
+    datetime = datetime.now()
+
+    if not title or not creature or not description:
+        abort(400)
+
+    post = Post(title, creature, datetime, description, user_id, likes, dislikes)
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect('/sightings')
 
 @app.get('/logout')
 def logout():
