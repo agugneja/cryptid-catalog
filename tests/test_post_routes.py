e8b9994c9c503197e5ba68dd7fabb7de6526d4ee
Alyssa Gugneja <alyssagugneja@gmail.com>
2022-12-14 10:53:16 -0500

    fixed two tests

--- tests/test_post_routes.py
+++ tests/test_post_routes.py
@@ -60,22 +60,54 @@ def test_create_comment(test_app: FlaskClient):
     db.session.add(test_comment)
     db.session.commit()
 
-    test_liked = comment_repository_singleton.add_comment_like(test_comment.comment_id, test_user.user_id)
-    db.session.add(test_liked)
-    db.session.commit()
-
     # Run action
     res = test_app.post(f'/comment/{test_sighting.post_id}')
-    res = test_app.post(f'/like/{test_sighting.post_id}/{test_comment.comment_id}')
 
     assert test_comment.post_id == test_sighting.post_id
     assert test_comment.text == "Elaborate. Please."
     assert test_comment.user_id == test_commenter.user_id
-    assert test_comment.likes == 1
+    assert test_comment.likes == 0
     assert test_comment.dislikes == 0
 
-# def test_like_comment(test_app: FlaskClient):
-#     pass
+def test_like_comment(test_app: FlaskClient):
+    # Setup
+    refresh_db()
+    test_user = Person(username="jdoe", password="password", email="jdoe@yahoo.com")
+    test_commenter = Person(username="MonsterMan", password="123abc", email="asmith@gmail.com")
+
+    db.session.add(test_user)
+    db.session.add(test_commenter)
+    db.session.commit()
+
+    test_sighting = Post(title='Monster Sighted', creature='Loch Ness Monster', 
+    user_id=test_user.user_id, place='Loch Ness', description='Saw the Loch Ness monster. Maybe.', 
+    photo_path='static/forum_post_pictures/placeholder.png', likes='3', dislikes='12')
+
+    db.session.add(test_sighting)
+    db.session.commit()
+
+
+    test_comment = Comment(post_id = test_sighting.post_id, text = "Elaborate. Please.", 
+    user_id = test_commenter.user_id, time_stamp = datetime.now(), likes = 0, dislikes = 0)
+
+    db.session.add(test_comment)
+    db.session.commit()
+
+    #test_liked = comment_repository_singleton.add_comment_like(test_comment.comment_id, test_user.user_id)
+    #db.session.add(test_liked)
+    #db.session.commit()
+
+    # Run action
+    resComment = test_app.post(f'/comment/{test_sighting.post_id}')
+    resLike = test_app.post(f'/like/{test_sighting.post_id}/{test_comment.comment_id}')
+    test_app.post('/login')
+    page_data = resLike.data.decode()
+
+    assert test_comment.post_id == test_sighting.post_id
+    assert test_comment.text == "Elaborate. Please."
+    assert test_comment.user_id == test_commenter.user_id
+    assert '<button type="submit" class="btn btn-success">&#8205; &#8205; &#8205; LIKE &#8205; &#8205; &#8205; </button> &#8205 1' in page_data
+    assert test_comment.dislikes == 0
 
 # def test_dislike_comment(test_app: FlaskClient):
 #     pass
--- tests/utils.py
+++ tests/utils.py
@@ -1,13 +1,11 @@
 from src.models import Post, Person, Comment, commentlikes, commentdislikes, postlikes, postdislikes, db
 from src.models import Post, Person, Comment, commentlikes, db
 def refresh_db():
-    Comment.query.delete()
-    Post.query.delete()
-    Person.query.delete()
     commentlikes.query.delete()
-
     commentdislikes.query.delete()
+    Comment.query.delete()
     postlikes.query.delete()
     postdislikes.query.delete()
-
+    Post.query.delete()
+    Person.query.delete()
     db.session.commit()
